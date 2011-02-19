e08b0daa488aaebb2989bfba9d3cd5a2893a82c3
Fabien Potencier <fabien.potencier@gmail.com>
2011-02-19 23:08:48 +0100

    updated vendors

--- app/config/routing_dev.php
+++ app/config/routing_dev.php
@@ -4,7 +4,7 @@ use Symfony\Component\Routing\RouteCollection;
 
 $collection = new RouteCollection();
 $collection->addCollection($loader->import(__DIR__.'/routing.php'));
-
+$collection->addCollection($loader->import("@WebProfilerBundle/Resources/config/routing/wdt.xml"), '/_wdt');
 $collection->addCollection($loader->import("@WebProfilerBundle/Resources/config/routing/profiler.xml"), '/_profiler');
 
 return $collection;
