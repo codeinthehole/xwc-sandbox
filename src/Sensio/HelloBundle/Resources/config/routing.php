6609e3fb25d6a83af574c43c9b5f981c51ea796d
Fabien Potencier <fabien.potencier@gmail.com>
2011-02-19 18:56:03 +0100

    added a welcome page

--- src/Sensio/HelloBundle/Resources/config/routing.php
+++ src/Sensio/HelloBundle/Resources/config/routing.php
@@ -4,6 +4,9 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
 $collection = new RouteCollection();
+$collection->add('homepage', new Route('/', array(
+    '_controller' => 'HelloBundle:Hello:welcome',
+)));
 $collection->add('hello', new Route('/hello/{name}', array(
     '_controller' => 'HelloBundle:Hello:index',
 )));
